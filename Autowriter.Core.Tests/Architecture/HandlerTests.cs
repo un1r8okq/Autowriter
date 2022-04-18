09f99fb4f938155b16b6f5ca06f880b68775fe77
Will Verland <will@ver.land>
2022-04-18 15:12:33 +1200

    Add tests for Handler architecture

---
+++ Autowriter.Core.Tests/Architecture/HandlerTests.cs
@@ -0,0 +1,64 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using Xunit;
+
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Autowriter.Core.Tests.Architecture
+{
+    public class HandlerTests
+    {
+        private static readonly ArchUnitNET.Domain.Architecture Architecture;
+        private static readonly IObjectProvider<Class> Handlers;
+        private static readonly IObjectProvider<Class> FeatureClasses;
+
+        static HandlerTests()
+        {
+            Architecture = new ArchLoader()
+                .LoadAssemblies(typeof(Autowriter.Core.ServiceCollectionExtensions).Assembly)
+                .Build();
+            Handlers = Classes().That().ImplementInterface("RequestHandler").As("Handlers");
+            FeatureClasses = Classes().That().ResideInNamespace("Autowriter.Core.Features");
+        }
+
+        [Fact]
+        public void HandlersShouldBeCalledHandler()
+        {
+            var rule = Classes().That().Are(Handlers)
+                .Should().HaveName("Handler");
+
+            rule.Check(Architecture);
+        }
+
+        [Fact]
+        public void ClassesCalledHandlerShouldBeHandlers()
+        {
+            var rule = Classes().That().HaveName("Handler")
+                .Should().Be(Handlers);
+
+            rule.Check(Architecture);
+        }
+
+        [Fact]
+        public void HandlersShouldBeNested()
+        {
+            var rule = Classes().That().Are(Handlers)
+                .Should().BeNested()
+                .Because("Nested classes are used to group a features command/query, handler, and response together");
+
+            rule.Check(Architecture);
+        }
+
+        [Fact]
+        public void HandlersShouldNotBeReferencedDirectly()
+        {
+            var rule = Classes().That().Are(FeatureClasses)
+                .And().AreNot(Handlers)
+                .Should().NotDependOnAny(Handlers)
+                .Because("Handlers are supposed to be abstract and resolved using the Moq library");
+
+            rule.Check(Architecture);
+        }
+    }
+}
