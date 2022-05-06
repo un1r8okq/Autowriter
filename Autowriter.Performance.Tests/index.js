9872908d032ff545cf2df2bb7894a9cd71ea9788
Will Verland <will@ver.land>
2022-05-06 20:15:26 +1200

    Refactor performance test

--- Autowriter.Performance.Tests/index.js
+++ Autowriter.Performance.Tests/index.js
@@ -12,51 +12,35 @@ export const options = {
 
 const baseUrl = 'http://localhost:5067';
 
-export default function () {
-    var email = `${getRandomString()}@example.com`;
-    var password = getRandomString();
-    register(email, password);
-    login(email, password);
+export function setup() {
+    const creds = {
+        email: `${randomString()}@example.com`,
+        password: randomString(),
+    };
 
-    var res = postProtectedForm(`${baseUrl}/generate`, { wordcount: 1000 });
+    post(`${baseUrl}/user/register`, creds);
+
+    return creds;
+}
+
+export default function(creds) {
+    post(`${baseUrl}/User/Login`, creds);
+
+    var res = post(`${baseUrl}/generate`, { wordcount: 1000 });
     check(res, { 'status was 200': (r) => r.status == 200 });
     check(res, { 'contains generated text': (r) => /<h2>Generated writing<\/h2>/.test(r.body) });
 }
 
-function getRandomString() {
+function randomString() {
     return Math.random().toString(16).substring(2, 16);
 }
 
-function register(email, password) {
-    const url = `${baseUrl}/User/Register`;
-    const res = http.get(url);
-    res.submitForm({
-        formSelector: 'form',
-        fields: {
-            Email: email,
-            Password: password,
-        },
-    });
-}
+function post(url, body) {
+    const res = http.get(`${baseUrl}/generate`);
+    const tokenExpr = /__RequestVerificationToken" type="hidden" value="(.+)"/;
+    const token = res.body.match(tokenExpr)[1];
 
-function login(email, password) {
-    const url = `${baseUrl}/User/Login`;
-    const res = http.get(url);
-    res.submitForm({
-        formSelector: 'form',
-        fields: {
-            email: email,
-            password: password,
-        },
-    });
-}
-
-function postProtectedForm(url, body) {
-    const getRes = http.get(`${baseUrl}/generate`);
-    const xsrfTokenRegex = /__RequestVerificationToken" type="hidden" value="(.+)"/;
-    const xsrfToken = getRes.body.match(xsrfTokenRegex)[1];
-
-    var postBody = { __RequestVerificationToken: xsrfToken };
+    var postBody = { __RequestVerificationToken: token };
     Object.assign(postBody, body);
     return http.post(url, postBody);
 }
